4a8a1235a0aa2a0cd15a2aaf69cc2457f5d7bcd9
devfer <ferkki@hotmail.com>
2020-05-31 23:36:37 -0400

    dark mode and light mode styles optimized

--- js/app.js
+++ js/app.js
@@ -2,14 +2,13 @@ const $checkbox = document.querySelector("#checkbox");
 
 if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
   $checkbox.setAttribute("checked", true);
+  document.body.classList.add("is-dark-mode");
 }
 
 $checkbox.addEventListener("change", function (event) {
   if (this.checked) {
     document.body.classList.add("is-dark-mode");
-    document.body.classList.remove("is-light-mode");
   } else {
-    document.body.classList.add("is-light-mode");
     document.body.classList.remove("is-dark-mode");
   }
 });
